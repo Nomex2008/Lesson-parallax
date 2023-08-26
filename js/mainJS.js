da4594515a2207bfb3290bade52fbff3570eb5b7
Alex <97978327+Nomex2008@users.noreply.github.com>
2023-08-26 23:29:44 +0300

    Commit 4 (scroll parallax)
    Add scroll parallax.

--- js/mainJS.js
+++ js/mainJS.js
@@ -4,7 +4,7 @@ window.onload = function () {
     const parallax = document.querySelector('.parallax')
 
     if(parallax){
-        const content = document.querySelector('.parallax__container')
+        const content = document.querySelector('.content')
         const clouds = document.querySelector('.images-parallax_clouds')
         const mountins = document.querySelector('.images-parallax_mountins')
         const humen = document.querySelector('.images-parallax_humen')
@@ -46,5 +46,31 @@ window.onload = function () {
             coordXprocent = coordX / parallaxWidth * 100
             coordYprocent = coordY / parallaxHeight * 100
         })
+
+        //parallax for scroll
+
+        let thresholdSets = [];
+        for (let i = 0; i <= 1.0; i+= 0.005) {
+            thresholdSets.push(i)
+        }
+
+
+        const callback = function () {
+            const scrollTopProcent = window.pageYOffset / parallax.offsetHeight * 100;
+            setParallaxItemsStyle(scrollTopProcent)
+        }
+
+        const observer = new IntersectionObserver(callback, {
+            threshold: thresholdSets
+        })
+        
+
+        observer.observe(document.querySelector('.content'))
+
+        function setParallaxItemsStyle(scrollTopProcent) {
+            content.style.cssText = `transform: translate(0%,-${scrollTopProcent/9}%)`
+            mountins.parentElement.style.cssText = `transform: translate(0%,-${scrollTopProcent/6}%)`
+            humen.parentElement.style.cssText = `transform: translate(0%,-${scrollTopProcent/3}%)`
+        }
     }
 }
